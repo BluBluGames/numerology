3e34f6850caba019db39eac95629a1133c63f741
Michal Ambrozy <ambrozy.main@gmail.com>
2025-01-12 07:10:20 +0100

    change numerology page to request flask

--- src/components/NumerologyPortrait.vue
+++ src/components/NumerologyPortrait.vue
@@ -2,7 +2,7 @@
   <div>
     <form @submit.prevent="calculateVibration">
       <div>
-        <label for="birth-date">Data urodzenia:</label>
+        <label for="birth-date">Date of Birth:</label>
         <input 
           type="date" 
           id="birth-date" 
@@ -12,77 +12,61 @@
       </div>
 
       <div>
-        <label for="full-name">Pełne imię i nazwisko:</label>
+        <label for="full-name">Full Name:</label>
         <input 
           type="text" 
           id="full-name" 
           v-model="fullName" 
-          placeholder="Wprowadź pełne imię i nazwisko"
+          placeholder="Enter your full name"
           required
         />
       </div>
 
       <div>
-        <label for="signature">Podpis:</label>
+        <label for="signature">Signature:</label>
         <input 
           type="text" 
           id="signature" 
           v-model="signature" 
-          placeholder="Wprowadź podpis"
+          placeholder="Enter your signature"
           required
         />
       </div>
 
-      <button type="submit">Oblicz Wibrację</button>
+      <button type="submit">Calculate Vibration</button>
     </form>
 
     <div v-if="vibrationResult !== null">
-      <h3>Wynik:</h3>
+      <h3>Result:</h3>
       <p>
-        Wibracja Urodzenia: {{ vibrationResult }}
-        <span v-if="isMasterNumber(vibrationResult)" >(Liczba Mistrzowska)</span>
+        Life Path Number: {{ vibrationResult }}
+        <span v-if="isMasterNumber(vibrationResult)"> (Master Number)</span>
       </p>
-      <p>
-        Wibracja Dnia Urodzenia: {{ dayVibration }}
-      </p>
-      <p>
-        Wibracja Wnętrza (samogłoski): {{ innerVibration }}
-      </p>
-      <p>
-        Wibracja Zewnętrzna (spółgłoski): {{ outerVibration }}
-      </p>
-      <p>
-        Wibracja Celów Życiowych: {{ lifeGoalsVibration }}
-      </p>
-      <p>
-        Wibracja Indywidualności: {{ individualityVibration }}
-      </p>
-      <p>
-        Wibracja Temperamentu i Uzdolnień:
-        <ul>
-          <li>Mentalne: {{ temperamentVibration.mental }}</li>
-          <li>Emocjonalne: {{ temperamentVibration.emotional }}</li>
-          <li>Intuicyjne: {{ temperamentVibration.intuitive }}</li>
-          <li>Artystyczne: {{ temperamentVibration.artistic }}</li>
-          <li>Naukowe: {{ temperamentVibration.scientific }}</li>
-          <li>Związane z prowadzeniem interesów: {{ temperamentVibration.business }}</li>
-          <li>Fizyczne: {{ temperamentVibration.physical }}</li>
-        </ul>
-      </p>
-      <p>
-        Wibracja Podpisu: {{ signatureVibration }}
-      </p>
-      <p>
-        Potencjalne Przeznaczenie: {{ potentialDestiny }}
-      </p>
-      <p>
-        Przeznaczenie: {{ destiny }}
-      </p>
+      <p>Day Vibration: {{ dayVibration }}</p>
+      <p>Inner Vibration (Vowels): {{ innerVibration }}</p>
+      <p>Outer Vibration (Consonants): {{ outerVibration }}</p>
+      <p>Life Goals Vibration: {{ lifeGoalsVibration }}</p>
+      <p>Individuality Vibration: {{ individualityVibration }}</p>
+      <p>Temperament and Talents:</p>
+      <ul>
+        <li>Mental: {{ temperamentVibration.mental }}</li>
+        <li>Emotional: {{ temperamentVibration.emotional }}</li>
+        <li>Intuitive: {{ temperamentVibration.intuitive }}</li>
+        <li>Artistic: {{ temperamentVibration.artistic }}</li>
+        <li>Scientific: {{ temperamentVibration.scientific }}</li>
+        <li>Business: {{ temperamentVibration.business }}</li>
+        <li>Physical: {{ temperamentVibration.physical }}</li>
+      </ul>
+      <p>Signature Vibration: {{ signatureVibration }}</p>
+      <p>Potential Destiny: {{ potentialDestiny }}</p>
+      <p>Destiny: {{ destiny }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "NumerologyPortrait",
   data() {
@@ -111,97 +95,40 @@ export default {
     };
   },
   methods: {
-    calculateVibration() {
-      if (!this.birthDate || !this.fullName || !this.signature) return;
-
-      // Zamiana daty na cyfry i sumowanie ich
-      const numbers = this.birthDate.replaceAll("-", "").split("").map(Number);
-      let sum = numbers.reduce((acc, num) => acc + num, 0);
-
-      // Redukcja do jednej cyfry lub liczby mistrzowskiej
-      while (sum > 9 && !this.isMasterNumber(sum)) {
-        sum = sum
-          .toString()
-          .split("")
-          .reduce((acc, num) => acc + parseInt(num), 0);
+    async calculateVibration() {
+      if (!this.birthDate || !this.fullName || !this.signature) {
+        alert("Please fill in all fields.");
+        return;
       }
 
-      this.vibrationResult = sum;
+      try {
+        const response = await axios.post('http://localhost:5000/api/numerology/calculate', {
+          birth_date: this.birthDate,
+          full_name: this.fullName,
+          signature: this.signature
+        });
 
-      // Pobranie dnia urodzenia jako wibracji dnia
-      const day = parseInt(this.birthDate.split("-")[2]);
-      this.dayVibration = day;
+        const data = response.data;
 
-      // Obliczanie wibracji imienia
-      const numerologyMap = {
-        A: 1, Ą: 1, B: 2, C: 3, Ć: 3, D: 4, E: 5, Ę: 5, F: 6, G: 7, H: 8, I: 9, J: 1,
-        K: 2, L: 3, Ł: 3, M: 4, N: 5, Ń: 5, O: 6, Ó: 6, P: 7, R: 9, S: 1, Ś: 1, T: 2,
-        U: 3, W: 5, Y: 7, Z: 8, Ź: 8, Ż: 8, V: 4, X: 6, Q: 8
-      };
-      const letters = this.fullName.replace(/\s+/g, "").toUpperCase().split("");
+        this.vibrationResult = data.vibration_result;
+        this.dayVibration = data.day_vibration;
+        this.innerVibration = data.inner_vibration;
+        this.outerVibration = data.outer_vibration;
+        this.lifeGoalsVibration = data.life_goals_vibration;
+        this.individualityVibration = data.individuality_vibration;
+        this.temperamentVibration = data.temperament_vibration;
+        this.signatureVibration = data.signature_vibration;
+        this.potentialDestiny = data.potential_destiny;
+        this.destiny = data.destiny;
 
-      const innerSum = letters
-        .filter((letter) => "AEIOUYĄĘÓ".includes(letter))
-        .map((letter) => numerologyMap[letter] || 0)
-        .reduce((acc, num) => acc + num, 0);
-
-      const outerSum = letters
-        .filter((letter) => !"AEIOUYĄĘÓ".includes(letter))
-        .map((letter) => numerologyMap[letter] || 0)
-        .reduce((acc, num) => acc + num, 0);
-
-      this.innerVibration = this.reduceToSingleDigit(innerSum);
-      this.outerVibration = this.reduceToSingleDigit(outerSum);
-
-      // Wibracja Celów Życiowych
-      this.lifeGoalsVibration = this.reduceToSingleDigit(innerSum + outerSum);
-
-      // Wibracja Indywidualności
-      const totalSum = letters
-        .map((letter) => numerologyMap[letter] || 0)
-        .reduce((acc, num) => acc + num, 0);
-      this.individualityVibration = this.reduceToSingleDigit(totalSum);
-
-      // Wibracja Temperamentu i Uzdolnień
-      const frequency = letters
-        .map((letter) => numerologyMap[letter] || 0)
-        .reduce((acc, num) => {
-          acc[num] = (acc[num] || 0) + 1;
-          return acc;
-        }, {});
-
-      this.temperamentVibration.mental = (frequency[1] || 0) + (frequency[7] || 0) + (frequency[8] || 0) + (frequency[9] || 0);
-      this.temperamentVibration.emotional = (frequency[2] || 0) + (frequency[3] || 0) + (frequency[6] || 0) + (frequency[9] || 0);
-      this.temperamentVibration.intuitive = (frequency[2] || 0) + (frequency[7] || 0) + (frequency[9] || 0);
-      this.temperamentVibration.artistic = (frequency[3] || 0) + (frequency[6] || 0) + (frequency[9] || 0);
-      this.temperamentVibration.scientific = (frequency[1] || 0) + (frequency[4] || 0) + (frequency[5] || 0) + (frequency[7] || 0) + (frequency[8] || 0) + (frequency[9] || 0);
-      this.temperamentVibration.business = (frequency[2] || 0) + (frequency[4] || 0) + (frequency[8] || 0);
-      this.temperamentVibration.physical = (frequency[4] || 0) + (frequency[5] || 0) + (frequency[8] || 0);
-
-      // Wibracja Podpisu
-      const signatureLetters = this.signature.replace(/\s+/g, "").toUpperCase().split("");
-      const signatureSum = signatureLetters
-        .map((letter) => numerologyMap[letter] || 0)
-        .reduce((acc, num) => acc + num, 0);
-      this.signatureVibration = this.reduceToSingleDigit(signatureSum);
-
-      // Potencjalne przeznaczenie
-      this.potentialDestiny = this.vibrationResult;
-
-      // Przeznaczenie
-      this.destiny = this.reduceToSingleDigit(this.vibrationResult + this.signatureVibration);
+      } catch (error) {
+        console.error("Error calculating vibration:", error);
+        alert("An error occurred while calculating the vibrations. Please try again." + error);
+      }
     },
+
     isMasterNumber(number) {
       return [11, 22, 33, 44].includes(number);
-    },
-    reduceToSingleDigit(number) {
-      while (number > 9 && !this.isMasterNumber(number)) {
-        number = number
-          .toString()
-          .split("")
-          .reduce((acc, num) => acc + parseInt(num), 0);
-      }
-      return number;
     },
   },
 };
